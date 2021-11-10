2a62e457eb279a41bf980034c23dd42f3f115cb0
idzyubin <idzyubin@yahoo.com>
2021-11-11 00:47:37 +0300

    add proxy and handler registration extensions

---
+++ NatsExtensions/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using JetBrains.Annotations;
+using Microsoft.Extensions.DependencyInjection;
+using NatsExtensions.Handlers;
+using NatsExtensions.HostedServices;
+using NatsExtensions.Models;
+using NatsExtensions.Proxies;
+
+namespace NatsExtensions.Extensions
+{
+    /// <summary>
+    ///     DI extensions
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        ///     Register NATS handler for handle request from external
+        /// </summary>
+        /// <param name="services"><see cref="IServiceCollection"/></param>
+        /// <typeparam name="TRequest"><see cref="Request"/></typeparam>
+        /// <typeparam name="TReply"><see cref="Reply"/></typeparam>
+        /// <typeparam name="THandler"><see cref="IHandler{TRequest,TReply}"/></typeparam>
+        /// <returns>The same service collection so that multiple calls can be chained.</returns>
+        public static IServiceCollection AddNatsHandler<TRequest, TReply, THandler>(this IServiceCollection services)
+            where TRequest : Request
+            where TReply : Reply
+            where THandler : class, IHandler<TRequest, TReply>
+        {
+            services.AddTransient<IHandler<TRequest, TReply>, THandler>();
+            services.AddHostedService<RegisterHandlerService<TRequest, TReply>>();
+            return services;
+        }
+
+        /// <summary>
+        ///     Register NATS proxy for isolate logic of sending request to external
+        /// </summary>
+        /// <param name="services"><see cref="IServiceCollection"/></param>
+        /// <typeparam name="TRequest"><see cref="Request"/></typeparam>
+        /// <typeparam name="TReply"><see cref="Reply"/></typeparam>
+        /// <typeparam name="TProxy"><see cref="IProxy{TRequest,TReply}"/></typeparam>
+        /// <returns>The same service collection so that multiple calls can be chained.</returns>
+        public static IServiceCollection AddNatsProxy<TRequest, TReply, TProxy>(this IServiceCollection services)
+            where TRequest : Request
+            where TReply : Reply
+            where TProxy : class, IProxy<TRequest, TReply>
+        {
+            services.AddTransient<IProxy<TRequest, TReply>, TProxy>();
+            return services;
+        }
+    }
+}
--- NatsExtensions/Handlers/IHandler.cs
+++ NatsExtensions/Handlers/IHandler.cs
@@ -6,17 +6,17 @@ namespace NatsExtensions.Handlers
     /// <summary>
     ///     Interface for nats handler
     /// </summary>
-    /// <typeparam name="TRequest">Request target type</typeparam>
-    /// <typeparam name="TResponse">Reply target type</typeparam>
-    public interface IHandler<TRequest, TResponse>
+    /// <typeparam name="TRequest"><see cref="Request"/></typeparam>
+    /// <typeparam name="TReply"><see cref="Reply"/></typeparam>
+    public interface IHandler<TRequest, TReply>
         where TRequest : Request
-        where TResponse : Reply
+        where TReply : Reply
     {
         /// <summary>
         ///     Handle received request
         /// </summary>
         /// <param name="request">Request from remote subject</param>
         /// <returns>Handler reply</returns>
-        Task<TResponse> Handle(TRequest request);
+        Task<TReply> Handle(TRequest request);
     }
 }
